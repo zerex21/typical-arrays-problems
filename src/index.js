9fbfa364312beaa1a59162bd836594a56ebab094
zerex21 <zerex21@icloud.com>
2020-02-25 21:22:49 +0300

    add:solution task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined || array[0] === undefined)
+        return 0;
+    return Math.min.apply(null, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array[0] === undefined)
+        return 0;
+    return Math.max.apply(null, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array[0] === undefined)
+        return 0;
+    for (var i = 0, sum = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    if (array == 0)
+        return 0
+
+    var avg = sum / array.length;
+    return avg;
 }
